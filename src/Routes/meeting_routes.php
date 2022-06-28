db0738e0279827cb62c4e5608641eced65f877ac
Abolfazl Norzad <69985608+abolfazlnorzad@users.noreply.github.com>
2022-06-28 13:25:35 +0430

    delete middleware from routes

--- src/Routes/meeting_routes.php
+++ src/Routes/meeting_routes.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Nrz\Meeting\Http\Controllers\MeetingController;
-
-Route::middleware("auth")->group(function () {
-    Route::apiResource("meeting", MeetingController::class)->parameters([
+Route::apiResource("meeting", MeetingController::class)->parameters([
         "meeting" => "meeting:meetingID"
     ]);
-    Route::post("join/{meeting:meetingID}", [MeetingController::class, "joinToMeeting"]);
-});
-
+Route::post("join/{meeting:meetingID}", [MeetingController::class, "joinToMeeting"]);
 Route::get("{meeting:meetingID}" , [MeetingController::class , "callback"]);
